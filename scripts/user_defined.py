f3f5d95950141f40ad68a608c1f31a424ff4fa00
yolandaPing <158555986+yolandaPing@users.noreply.github.com>
2025-02-11 12:00:08 +0800

    Add files via upload

---
+++ scripts/user_defined.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# .FileName:user-defined
+# .Date....:2024-04-25 : 22 :43
+# .@Author:You P
+# .
+# .Finish time:
+
+# import hotKey
+hotkey = False
+
+# Allow_users: enable smooth copy weight
+Allow_users = True
+
+# Allow_users: enable smooth copy weight
+Allow_smooth = True
+
+# Grp_prisec: add ctrl sec and pri group
+Grp_prisec = False
+
+#rename tool suffix
+suffix = "bind"
+
+# enable Chinese
+Chinese = True
+
+# copy tool ui
+CopyTools_UI_new = True
+
+# optional drive tool ui
+Optional_Drive_UI_new = True
+
+# blendshape Editor designated users use blendshape
+Blendshape_Editor_User = False
+
+# dirve pose tool: enable mirror
+Dirve_Pose_Mirror = False
+
