dbd826adc98e2b7ac83f3766dbf6a2b037e51f69
yolandaPing <158555986+yolandaPing@users.noreply.github.com>
2025-02-11 13:07:33 +0800

    Add files via upload

---
+++ scripts/py27/saveRootPath.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# .FileName:saveRootPath
+# .Date....:2023-01-09 : 16 :37
+# .@Author:You P
+# .
+# .Finish time:
+
+
+import os
+
+
+ParentPath = os.path.split(__file__)[0].split('scripts')[0]
+ScriptsPath = os.path.split(__file__)[0].split(' ')[0]
+
+# print(ParentPath,ScriptsPath)
+
